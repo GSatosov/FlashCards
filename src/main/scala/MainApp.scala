b25d9dbe3c45ba2437210a56929b81736ce199ff
Georgiy Satosov <gsatosov@gmail.com>
2017-12-28 15:57:23 +0300

    Trying to set the correct host

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -17,7 +17,8 @@ object MainApp extends App {
   val httpService = new HttpService
   var port = scala.util.Properties.envOrElse("PORT", "8080").toInt
   println(port)
-  Http().bindAndHandle(httpService.routes, "localhost", port)
+  var http = scala.util.Properties.envOrElse("HOST", "localhost")
+  Http().bindAndHandle(httpService.routes, http, port)
 
 }
 
